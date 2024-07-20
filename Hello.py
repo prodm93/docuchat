9ad87ac8841e9cba660b14acd140597574b34f68
prodm93 <105621504+prodm93@users.noreply.github.com>
2024-07-20 09:50:52 +0000

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -7,22 +7,22 @@ os.system('pip install -U flash_attn==2.6.1 --no-build-isolation')
 
 st.title("LLM-Powered Document Chat")
 
-with st.expander('Guideline Inputs'):
-    with st.sidebar.form(key='guidelines_form', clear_on_submit=False):
-        st.write("Upload your PDF documents here:")
-        documents = st.file_uploader('Documents', type=['pdf'], accept_multiple_files=True, key='doc_pdf', help=None, label_visibility="visible")
-        hf_api_token = st.text_input("Enter your HuggingFace access token/API key:", type='password')
-        submitted = st.form_submit_button(label="Submit", help=None, on_click=None, type="secondary", disabled=False)
-        if submitted:
-            st.session_state.documents = documents
-            st.session_state.hf_api_token = hf_api_token
-            path = "./pdf_images"
-            fname_list = [PDFExtractor(path, x) for x in documents if x.endswith('.pdf')]
-            all_texts, all_tables = [], []
-            for obj in fname_list:
-                texts, tables = obj.categorize_elements()
-                all_texts.extend(texts)
-                all_tables.extend(tables)
+
+with st.sidebar.form(key='guidelines_form', clear_on_submit=False):
+    st.write("Upload your PDF documents here:")
+    documents = st.file_uploader('Documents', type=['pdf'], accept_multiple_files=True, key='doc_pdf', help=None, label_visibility="visible")
+    hf_api_token = st.text_input("Enter your HuggingFace access token/API key:", type='password')
+    submitted = st.form_submit_button(label="Submit", help=None, on_click=None, type="secondary", disabled=False)
+    if submitted:
+        st.session_state.documents = documents
+        st.session_state.hf_api_token = hf_api_token
+        path = "./pdf_images"
+        fname_list = [PDFExtractor(path, x) for x in documents if x.endswith('.pdf')]
+        all_texts, all_tables = [], []
+        for obj in fname_list:
+            texts, tables = obj.categorize_elements()
+            all_texts.extend(texts)
+            all_tables.extend(tables)
 
     with st.form(key='inference_form', clear_on_submit=False):
         question = st.text_input('Question: ')
